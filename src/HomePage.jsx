379504092ba98d0778060d1da7a8f5be3d56711d
Mikey <michaeljpkerr@gmail.com>
2025-09-26 13:43:17 +1000

    update homepage styles - WIP

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -19,6 +19,8 @@ export default function HomePage() {
         uses the HTML5 history API to keep your UI in sync with the URL.
       </p>
 
+      <br />
+
       <p>
         Inside <code> &lt;BrowserRouter&gt; </code>, you define routes using the
         <code> &lt;Routes&gt; </code> and <code> &lt;Route&gt; </code>{" "}
@@ -26,26 +28,25 @@ export default function HomePage() {
         components in a clean and readable way.
       </p>
 
+      <br />
+      <hr className="border-t border-gray-300" />
+
       <h2 className="mt-3">Example</h2>
-
-      <pre>
-        <code>{`
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-        `}</code>
-      </pre>
+      <div class=" bg-[#1e1e1e] text-sm font-mono rounded-lg p-4 overflow-x-auto">
+        <pre class="bg-[#1e1e1e]">
+          <code>
+            <span class="text-purple-400">import</span>{" "}
+            <span className="text-yellow-400">
+              {"{"} <span className="text-blue-400">browser</span> {"}"}{" "}
+            </span>
+            <span class="text-purple-400">from</span>{" "}
+            <span class="text-orange-300">
+              'react-router-dom'<span className="text-white">;</span>
+            </span>
+            <br />
+          </code>
+        </pre>
+      </div>
     </motion.div>
   );
 }
